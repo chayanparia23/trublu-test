ddc05c910f624d99de5c69a75dacfe84ce435639
Chayan Paria <chayan.paria@instem.com>
2024-07-03 16:59:22 +0530

    Changes

--- src/app/modules/user-dashboard/modules/user/components/users/users.component.ts
+++ src/app/modules/user-dashboard/modules/user/components/users/users.component.ts
@@ -1,21 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
+import { BehaviorSubject, Subscription, combineLatest, debounceTime, fromEvent, map } from 'rxjs';
+import { Role } from 'src/app/modules/shared/enums/role.enum';
 import { UsersService } from 'src/app/modules/shared/services/data-services/users.service';
 import { ConfirmService } from 'src/app/modules/shared/services/util-services/confirm.service';
+import { addSubscriptionsToSubscription } from 'src/app/modules/shared/utils/addToSubscriptionArray.utils';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.scss']
 })
-export class UsersComponent implements OnInit {
+export class UsersComponent implements OnInit, OnDestroy {
 
-  users$ = this.usersService.users$;
+  selectedRoleFilter$ = new BehaviorSubject<string>(null);
+  users$ = combineLatest([
+    this.usersService.users$,
+    this.selectedRoleFilter$
+  ]).pipe(map(([users, selectedRole]) => {
+    
+    if(selectedRole) {
+      return users.filter(user => user.role === selectedRole);
+    }
+    
+    return users;
+  }));
+
+  roles = this.enumToKeyValueArray(Role);
+
+  
+  subscriptions = new Subscription();
 
   constructor(private usersService: UsersService, private router: Router, private confirmService: ConfirmService) {}
 
   ngOnInit(): void {
-    this.usersService.checkAndGetUsers();
+    addSubscriptionsToSubscription(this.subscriptions, [
+      this.usersService.checkAndGetUsers()
+    ])
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
   }
 
   removeUser(userId: string, name: string) {
@@ -25,6 +50,17 @@ export class UsersComponent implements OnInit {
       }
     })
     
+  }
+
+  enumToKeyValueArray(enumObj: any): { key: string; value: any }[] {
+    return Object.keys(enumObj).map(key => ({
+      key: key,
+      value: enumObj[key]
+    }));
+  }
+
+  setRoleFilter(role: string) {
+    this.selectedRoleFilter$.next(role);
   }
 
   viewUser(userId: string) {
